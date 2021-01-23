38749d56063b48e0410f832c134de28a4e3b1f9d
Renato Nakano <renato.sousa.gc@hotmail.com>
2021-01-22 21:21:27 -0300

    Finalizada tela de detalhes

--- GerenciaDeImoveis/Telas/Detalhes/Detalhes.Designer.cs
+++ GerenciaDeImoveis/Telas/Detalhes/Detalhes.Designer.cs
@@ -29,34 +29,106 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBox_Observacoes = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(40)))), ((int)(((byte)(102)))));
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(602, 258);
+            this.panel1.Size = new System.Drawing.Size(502, 443);
             this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(40)))), ((int)(((byte)(102)))));
+            this.panel3.Controls.Add(this.richTextBox_Observacoes);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(0, 304);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(502, 139);
+            this.panel3.TabIndex = 1;
+            // 
+            // richTextBox_Observacoes
+            // 
+            this.richTextBox_Observacoes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(184)))), ((int)(((byte)(255)))));
+            this.richTextBox_Observacoes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox_Observacoes.CausesValidation = false;
+            this.richTextBox_Observacoes.Font = new System.Drawing.Font("Gill Sans MT", 12F);
+            this.richTextBox_Observacoes.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox_Observacoes.HideSelection = false;
+            this.richTextBox_Observacoes.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox_Observacoes.Name = "richTextBox_Observacoes";
+            this.richTextBox_Observacoes.ReadOnly = true;
+            this.richTextBox_Observacoes.Size = new System.Drawing.Size(478, 87);
+            this.richTextBox_Observacoes.TabIndex = 100;
+            this.richTextBox_Observacoes.TabStop = false;
+            this.richTextBox_Observacoes.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(40)))), ((int)(((byte)(102)))));
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 15F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(184)))), ((int)(((byte)(255)))));
+            this.label2.Location = new System.Drawing.Point(1, -6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(502, 43);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Observações";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Location = new System.Drawing.Point(-9, -9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(520, 109);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 16F);
+            this.label1.Location = new System.Drawing.Point(33, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 1;
             // 
             // Detalhes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 606);
+            this.ClientSize = new System.Drawing.Size(502, 443);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Detalhes";
             this.Text = "Detalhes";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox_Observacoes;
     }
 }
--- GerenciaDeImoveis/Telas/Detalhes/Detalhes.cs
+++ GerenciaDeImoveis/Telas/Detalhes/Detalhes.cs
@@ -3,18 +3,174 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GerenciaDeImoveis.Model;
 
 namespace GerenciaDeImoveis
 {
     public partial class Detalhes : Form
     {
-        public Detalhes()
+        private Imovel Imovel { get; set; }
+
+        public Detalhes(Imovel i)
         {
+            Imovel = i;
             InitializeComponent();
+            InitScreen();
+        }
+
+        public void InitScreen()
+        {
+            Label enderecoBairro = new Label();
+            Label preco = new Label();
+            Label terreno = new Label();
+            Label areaConstruida = new Label();
+            Label nGaragens = new Label();
+            Label nDorm = new Label();
+            Label estilo = new Label();
+            Label status = new Label();
+            Label indicacao = new Label();
+            Label ic1 = new Label();
+            Label ic2 = new Label();
+            Label ic3 = new Label();
+
+            FlowLayoutPanel f1 = new FlowLayoutPanel();
+            FlowLayoutPanel f2 = new FlowLayoutPanel();
+            FlowLayoutPanel f3 = new FlowLayoutPanel();
+
+            enderecoBairro.Location = new Point(0, 7);
+            enderecoBairro.Name = "enderecoBairro";
+            enderecoBairro.Size = new Size(518, 59);
+            enderecoBairro.ForeColor = Color.White;
+            enderecoBairro.TextAlign = ContentAlignment.MiddleCenter;
+            enderecoBairro.Text = Imovel.Endereco + " (" + Imovel.Bairro.ToString().Replace("_", " ") + ")";
+            enderecoBairro.Font = PreencherStringNoLabel(enderecoBairro);
+            
+            preco.Font = new Font("Gill Sans MT", 17F);
+            preco.Location = new Point(0, 44);
+            preco.Name = "preco";
+            preco.Size = new Size(502, 62);
+            preco.ForeColor = Color.White;
+            preco.TextAlign = ContentAlignment.MiddleCenter;
+            preco.Text = "R$ " + Imovel.Preco.ToString("N2");
+
+            panel2.Controls.Add(enderecoBairro);
+            panel2.Controls.Add(preco);
+
+            terreno.Font = new Font("Gill Sans MT", 14F);
+            terreno.Location = new Point(30, 120);
+            terreno.Name = "terreno";
+            terreno.AutoSize = true;
+            terreno.ForeColor = Color.White;
+            terreno.Text = "Terreno: " + Imovel.Terreno.ToString().PadLeft(18) + " m²";
+
+            areaConstruida.Font = new Font("Gill Sans MT", 14F);
+            areaConstruida.Location = new Point(30, 165);
+            areaConstruida.Name = "areaConstruida";
+            areaConstruida.AutoSize = true;
+            areaConstruida.ForeColor = Color.White;
+            areaConstruida.Text = "Área construída: " + Imovel.AreaConstruida.ToString().PadLeft(6) + " m²";
+
+            nDorm.Font = new Font("Gill Sans MT", 14F);
+            nDorm.Location = new Point(30, 210);
+            nDorm.Name = "nDorm";
+            nDorm.AutoSize = true;
+            nDorm.ForeColor = Color.White;
+            nDorm.Text = "Dormitórios: " + Imovel.nDormitorios.ToString().PadLeft(10);
+
+            nGaragens.Font = new Font("Gill Sans MT", 14F);
+            nGaragens.Location = new Point(30, 255);
+            nGaragens.Name = "nGaragens";
+            nGaragens.AutoSize = true;
+            nGaragens.ForeColor = Color.White;
+            nGaragens.Text = "Garagens: " + Imovel.nGaragens.ToString().PadLeft(15);
+
+            ic1.Image = Properties.Resources.check;
+            ic1.Size = new Size(16, 16);
+            ic1.Anchor = AnchorStyles.Left;
+            ic1.Name = "ic";
+
+            ic2.Image = Properties.Resources.check;
+            ic2.Size = new Size(16, 16);
+            ic2.Anchor = AnchorStyles.Left;
+            ic2.Name = "ic";
+
+            ic3.Image = Properties.Resources.check;
+            ic3.Size = new Size(16, 16);
+            ic3.Anchor = AnchorStyles.Left;
+            ic3.Name = "ic";
+
+            f1.Location = new Point(300, 120);
+            f1.Name = "f1";
+            f1.Size = new Size(250, 30);
+            f1.FlowDirection = FlowDirection.LeftToRight;
+            f1.Controls.Add(ic1);
+
+            f2.Location = new Point(300, 187);
+            f2.Name = "f2";
+            f2.Size = new Size(250, 30);
+            f2.FlowDirection = FlowDirection.LeftToRight;
+            f2.Controls.Add(ic2);
+
+            f3.Location = new Point(300, 255);
+            f3.Name = "f3";
+            f3.Size = new Size(250, 30);
+            f3.FlowDirection = FlowDirection.LeftToRight;
+            f3.Controls.Add(ic3);
+
+            estilo.Font = new Font("Gill Sans MT", 14F);
+            estilo.Location = new Point(300, 120);
+            estilo.Name = "estilo";
+            estilo.AutoSize = true;
+            estilo.ForeColor = Color.White;
+            estilo.Text = Imovel.Estilo.ToString();
+            f1.Controls.Add(estilo);
+
+            status.Font = new Font("Gill Sans MT", 14F);
+            status.Location = new Point(300, 187);
+            status.Name = "status";
+            status.AutoSize = true;
+            status.ForeColor = Color.White;
+            status.Text = Imovel.Status.ToString().Replace("_", " ");
+            f2.Controls.Add(status);
+
+            indicacao.Font = new Font("Gill Sans MT", 14F);
+            indicacao.Location = new Point(300, 255);
+            indicacao.Name = "indicacao";
+            indicacao.AutoSize = true;
+            indicacao.ForeColor = Color.White;
+            indicacao.Text = "Indicação: " + Imovel.Indicacao.ToString();
+            f3.Controls.Add(indicacao);
+
+            richTextBox_Observacoes.Text = Imovel.Observacoes;
+            
+
+            panel1.Controls.Add(terreno);
+            panel1.Controls.Add(areaConstruida);
+            panel1.Controls.Add(nDorm);
+            panel1.Controls.Add(nGaragens);
+            panel1.Controls.Add(f1);
+            panel1.Controls.Add(f2);
+            panel1.Controls.Add(f3);
+        }
+
+        public Font PreencherStringNoLabel(Label label)
+        {
+            float f = 17;
+            Font font = new Font("Gill Sans MT", 17);
+            Size s = TextRenderer.MeasureText(label.Text, font);
+
+            while (s.Width > 510)
+            {
+                f -= 0.5F;
+                font = new Font(label.Font.Name, f);
+                s = TextRenderer.MeasureText(label.Text, font);
+            }
+
+            return font;
         }
     }
 }
